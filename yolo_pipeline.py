9a6ebd624dc7f70bd0c7756a21a9f46f2491af45
Fawaz Ahmed <iamfauz@yahoo.com>
2021-04-04 20:09:23 +0400

    Added wait functionality to threads

--- yolo_pipeline.py
+++ yolo_pipeline.py
@@ -1,15 +1,16 @@
 import cv2
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, wait
 import threading
 
 # Function thaat takes in frame and proccess frames
 def process_frame(camera, frame):
     
-    # TODO - Run Yolo model here
+    #TODO - Run Yolo model here
     
     # Right now displaying frame for testing purposes
     cv2.imshow(f'Camera{camera}', frame) 
     cv2.waitKey(1) 
+    return f'Camera{camera}'
 
 
 def start_proccesor():
@@ -24,10 +25,19 @@ def start_proccesor():
         ret, frame1 = vcap1.read()
         ret, frame2 = vcap2.read()
         
+        task_futures = []
         # Start Proccessing frames in different threads
-        task1 = executor.submit(process_frame(1, frame1))
-        task2 = executor.submit(process_frame(2, frame2))
-       
+        task1 = executor.submit(process_frame, 1, frame1)
+        task2 = executor.submit(process_frame, 2, frame2)
+        
+        # Wait for all futures to complete
+        task_futures = []
+        task_futures.append(task1)
+        task_futures.append(task2)
+        wait(task_futures)
+                
+        print(task1.result())
+        print(task2.result())    
 
 
 if __name__ == '__main__':
